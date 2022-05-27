678b15b1aef06c9fac94477c686edf4cbef21f9c
Rafael Gomes <eng.rsgomes@gmail.com>
2022-05-27 18:00:39 -0300

    first commit

---
+++ pages/api/sendMail.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// export default (request: NextApiRequest, response: NextApiResponse) => {
+//   const { email } = request.body;
+
+//   return response.json({ email: `${email}` });
+// };
+
+// import { api, params } from "@serverless/cloud";
+import sendGridMail from "@sendgrid/mail";
+
+sendGridMail.setApiKey(
+  "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"
+);
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  const { email } = request.body;
+
+  const msg = {
+    to: "eng.rsgomes@gmail.com",
+    from: "eng.rsgomes@gmail.com",
+    subject: `Prazer ${email}`,
+    text: "foi?",
+  };
+
+  try {
+    await sendGridMail.send(msg);
+    response.send("Email sent successfully");
+  } catch (error: any) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+    response.status(500).json("Failed to send email");
+  }
+};
