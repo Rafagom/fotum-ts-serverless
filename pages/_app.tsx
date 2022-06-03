f6870880193d38e63eb4d05f1f63d136fbcaadfc
Rafael Gomes <eng.rsgomes@gmail.com>
2022-06-03 14:37:31 -0300

    react-gtm-module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-5TJPJLF" });
+  }, []);
   return <Component {...pageProps} />;
 }
 
